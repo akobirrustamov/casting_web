ee563d603ac9c6532094b28eb18632acc697c8da
Jumayev Diyorbek <4415.diyor@gmail.com>
2025-09-15 18:25:13 +0500

    ready project last version full

--- frontend/src/admin/admin/AdminHome.js
+++ frontend/src/admin/admin/AdminHome.js
@@ -60,7 +60,7 @@ const AdminHome = () => {
     const donutSegments = calculateDonutSegments();
 
     return (
-        <div className="min-h-screen bg-black text-white">
+        <div className="min-h-screen bg-black text-white pt-24">
             <Header props='admin' />
             <div className="container mx-auto px-4 py-8">
                 <h1 className="text-3xl font-bold text-center mb-8 text-blue-400">Admin Statistikasi</h1>
